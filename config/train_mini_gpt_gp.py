63f83e129007b4e896ca0cb9f59b2fb853f8bcc7
Mikolaj Pacek <miki.pacman@gmail.com>
2024-03-06 18:58:20 +0100

    readme and config

--- config/train_mini_gpt_gp.py
+++ config/train_mini_gpt_gp.py
@@ -1,7 +1,7 @@
 # train a miniature character-level shakespeare model
 # good for debugging and playing on macbooks and such
 
-out_dir = 'out-mini-gpt-gp'
+out_dir = 'out-mini-gpt-gp-8192'
 eval_interval = 250 # keep frequent because we'll overfit
 eval_iters = 200
 log_interval = 10 # don't print too too often
@@ -11,12 +11,12 @@ always_save_checkpoint = False
 
 wandb_log = True # override via command line if you like
 wandb_project = 'owt'
-wandb_run_name = 'mini-gpt-gp'
+wandb_run_name = 'mini-gpt-gp-8192'
 
 dataset = 'gp'
-gradient_accumulation_steps = 1
-batch_size = 64
-block_size = 256 # context of up to 256 previous characters
+gradient_accumulation_steps = 64
+batch_size = 1
+block_size = 8192
 
 # baby GPT model :)
 n_layer = 6
@@ -25,8 +25,8 @@ n_embd = 384
 dropout = 0.2
 
 learning_rate = 1e-3 # with baby networks can afford to go a bit higher
-max_iters = 5000
-lr_decay_iters = 5000 # make equal to max_iters usually
+max_iters = 50000
+lr_decay_iters = 50000 # make equal to max_iters usually
 min_lr = 1e-4 # learning_rate / 10 usually
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
 
